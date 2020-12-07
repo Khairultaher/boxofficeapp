f076350f8684a4584c45166d8fedf61f25d7d411
khairul <khairultaher@gmail.com>
2020-12-08 01:20:46 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navs from "./components/Navs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          This is home page !!
+        </Route>
+        <Route path="/starred">This is home page !! </Route>
+        <Route>404 ! page not found!!</Route>
+      </Switch>
     </div>
   );
 }
