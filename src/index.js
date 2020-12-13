e1c3d3a77773df799e8b86ef926fbc7cb5b1d162
khairul <khairultaher@gmail.com>
2020-12-14 00:45:24 +0600

    2020-12-13 1.1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-// import * as serviceWorker from './serviceworker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { swdev } from "./swdev";
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// serviceWorker.register();
+swdev();
---
+++ src/swdev.js
@@ -0,0 +1,6 @@
+export function swdev(){
+  let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+  navigator.serviceWorker.register(swUrl).then((res) => {
+    console.warn("res: ", res);
+  });
+}
